69f722a3d9df33df3c6f54bb5b1cd69949a70b7e
Andrew-J-Miller <32602661+Andrew-J-Miller@users.noreply.github.com>
2018-05-13 20:17:17 -0700

    Add files via upload
    Final version of crawl.py. Only searches 1 layer deep

--- crawl.py
+++ crawl.py
@@ -1,40 +1,37 @@
 import urllib2
 import re
 import os
+import sys
 
-#This is the worker python program that searches 2 links deep given a starting link and writes a text file of all links it finds to return to the master
+#crawl.py
+#This is the worker python script that takes a link and writes a text file of all links in that layer
 
 
-cwd = os.getcwd()
-location = cwd + "//test.txt"
+
 try:
-    website = urllib2.urlopen('http://www.starwars.wikia.com/Main_Page')
+	#Open website
+    website = urllib2.urlopen(sys.argv[1])
     html = website.read()
+	#A regular expression to find all https or http href links within the layer
     files = re.findall('href="http.*?"',html)
-    linkFile = open(location, 'w+')
+	#Open the outfile specified by the master
+    linkFile = open(sys.argv[2], 'w+')
 
 
     for x in files:
+		#A regular expression to format the links to just the link portion
         p = re.compile('http.+?(?=")')
         x = p.search(x)
         x = x.group()
+		#Write links to outfile
         linkFile.write(x)
         linkFile.write('\n')
-        try:
-            website2 = urllib2.urlopen(x)
-            html = website2.read()
-            files2 = re.findall('href="http.*?"',html)
-        except:
-            print x, " broken link"
+        
 
-        for y in files2:
-            p = re.compile('http.+?(?=")')
-            y = p.search(y)
-            y = y.group()
-            linkFile.write(y)
-            linkFile.write('\n')
+#condition for failure to open link passed to the worker			
 except:
     print "Starting link is broken. Exiting worker."
     exit()
+
 
 linkFile.close()
